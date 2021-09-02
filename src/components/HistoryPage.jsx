001fac372f0d2f036a2fc553023778b27a3b9792
HAMZA AIT OUMGHAR <hamza.aitoumghar.3@gmail.com>
2021-09-02 23:22:03 +0100

    update HistoryPage

--- src/components/HistoryPage.jsx
+++ src/components/HistoryPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Chart from "chart.js/auto";
 import { optionChart } from "../util/chartConfig";
 import "chartjs-adapter-moment";
@@ -6,6 +6,21 @@ const HistoryPage = ({ data }) => {
   const chartRef = useRef();
 
   const { day, week, year, detail } = data;
+
+  const [timeFormat, setTimeFormat] = useState("24h");
+
+  const determinseTimeFomrat = () => {
+    switch (timeFormat) {
+      case "24h":
+        return day;
+      case "7d":
+        return week;
+      case "1y":
+        return year;
+      default:
+        return day;
+    }
+  };
 
   useEffect(() => {
     let chartInstance = null;
@@ -17,7 +32,7 @@ const HistoryPage = ({ data }) => {
             {
               label: `${detail.name} price`,
               //label: `prices`,
-              data: day,
+              data: determinseTimeFomrat(),
               backgroundColor: "rgba(174,305,194,0.5)",
               borderColor: "rgba(174,305,194,0.4)",
               borderWidth: 1,
@@ -35,13 +50,56 @@ const HistoryPage = ({ data }) => {
         chartInstance.destroy();
       }
     };
-  }, []);
+  }, [timeFormat]);
+
+  const renderPrice = () => {
+    if (detail) {
+      const pclassName =
+        (detail.price_change_24h < 0
+          ? "text-danger my-0"
+          : "text-success my-0") + " fw-bolder";
+      return (
+        <>
+          <p className="my-0">{detail.current_price.toFixed(2)}$</p>
+          <p className={pclassName}>
+            {detail.price_change_percentage_24h.toFixed(2)}
+          </p>
+        </>
+      );
+    }
+  };
 
   return (
     <div className="bg-white border mt-2 rounded p-3">
-      <div></div>
+      <div>{renderPrice()}</div>
       <div>
         <canvas ref={chartRef} id="myChart" width={250} height={250}></canvas>
+      </div>
+      <div className="chart-button mt-1">
+        <div
+          className="btn btn-outline-secondary btn-sm"
+          onClick={() => {
+            setTimeFormat("24h");
+          }}
+        >
+          24h
+        </div>
+        <div
+          className="btn btn-outline-secondary btn-sm mx-1"
+          onClick={() => {
+            setTimeFormat("7d");
+          }}
+        >
+          7d
+        </div>
+        <div
+          className="btn btn-outline-secondary btn-sm"
+          onClick={() => {
+            setTimeFormat("1y");
+          }}
+        >
+          1y
+        </div>
       </div>
     </div>
   );
